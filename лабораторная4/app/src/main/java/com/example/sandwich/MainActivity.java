096a3d6cd3364f81a9573f114a9f2213d5d12e44
IES19991021 <erina.1r@yandex.ru>
2020-11-24 18:11:58 +0700

    Создала класс Cat и выполнила конвертацию экземпляра этого класса в JSON

--- лабораторная4/app/src/main/java/com/example/sandwich/MainActivity.java
+++ лабораторная4/app/src/main/java/com/example/sandwich/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.sandwich;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.graphics.Color;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Cat murzik = new Cat();
+        murzik.name = "Мурзик";
+        murzik.age = 9;
+        murzik.color = Color.RED;
+
+        //Конвертируем JSON в объект
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        Log.i("GSON", gson.toJson(murzik));
     }
 }
