67a2a833f37b8055b25b18b9dedd139bf526af72
IES19991021 <erina.1r@yandex.ru>
2020-12-10 10:03:06 +0700

    проект не до конца

--- sandwich-club-starter-code-master/app/src/main/java/com/udacity/sandwichclub/MainActivity.java
+++ sandwich-club-starter-code-master/app/src/main/java/com/udacity/sandwichclub/MainActivity.java
@@ -1,42 +1,49 @@
 package com.udacity.sandwichclub;
 
-import android.content.Intent;
 import android.os.Bundle;
-//import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.udacity.sandwichclub.model.Sandwich;
+import com.udacity.sandwichclub.utils.JsonUtils;
+
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private List<Sandwich> SandwichList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String[] sandList = getResources().getStringArray(R.array.sandwich_details);
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_names);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, sandwiches);
+        RecyclerView rv_view = findViewById(R.id.sand_list);
 
-        // Simplification: Using a ListView instead of a RecyclerView
-        ListView listView = findViewById(R.id.sandwiches_listview);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                launchDetailActivity(position);
+        SandwichList = new ArrayList<>();
+        for (int i = 0; i < sandwiches.length; i++) {
+            String json = sandList[i];
+            Sandwich sandwich = null;
+            try {
+                sandwich = JsonUtils.parseSandwichJson(json);
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-        });
-    }
+            SandwichList.add(sandwich);
+        }
 
-    private void launchDetailActivity(int position) {
-        Intent intent = new Intent(this, DetailActivity.class);
-        intent.putExtra(DetailActivity.EXTRA_POSITION, position);
-        startActivity(intent);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        rv_view.setLayoutManager(mLayoutManager);
+
+         SandAdapter = new SandAdapter(SandwichList);
+        rv_view.setAdapter(adapter);
     }
 }
-
-
