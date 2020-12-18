cf53948675d010b8f06edf9090ec1c276a1b724d
IES19991021 <erina.1r@yandex.ru>
2020-12-18 16:30:08 +0700

    готовый проект

---
+++ лабораторная 2/app/src/main/java/com/example/recyclerapp/DetailActivity.java
@@ -0,0 +1,21 @@
+package com.example.recyclerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        Bundle arguments = getIntent().getExtras();
+        String name = arguments.get("ARG_ITEM_ID").toString();
+
+        TextView textView = findViewById(R.id.details);
+        textView.setText(name);
+    }
+}
