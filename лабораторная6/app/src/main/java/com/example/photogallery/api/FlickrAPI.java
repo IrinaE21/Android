e6aa6fdef547a39093d191122cbeda40f85f6e51
IES19991021 <erina.1r@yandex.ru>
2020-12-25 02:56:45 +0700

    Создайте папку api, в ней создала интерфейс FlickrAPI и класс ServiceAPI:

---
+++ лабораторная6/app/src/main/java/com/example/photogallery/api/FlickrAPI.java
@@ -0,0 +1,17 @@
+package com.example.photogallery.api;
+
+
+import com.example.photogallery.model.Photo;
+import com.example.photogallery.model.PhotosResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+
+public interface FlickrAPI {
+    @GET("services/rest/?method=flickr.photos.getRecent&api_key=11f229c1e22485d9973068505c9c4f85&extras=url_s&format=json&nojsoncallback=1")
+    Call<PhotosResponse> getRecent();
+    @GET("services/rest/?method=flickr.photos.search&api_key=11f229c1e22485d9973068505c9c4f85&extras=url_s&format=json&nojsoncallback=1")
+    Call<PhotosResponse> getSearchPhotos(@Query("text") String keyWord);
+}
